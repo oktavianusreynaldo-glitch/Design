9d61db4dcad361aef709c261b6c4cb2475bf9d29
oktavianusreynaldo-glitch <oktavianusreynaldo@gmail.com>
2026-03-30 13:30:44 +0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2",
+      "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-2",
       {
         method: "POST",
         headers: {
